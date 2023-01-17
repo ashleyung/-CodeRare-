cb6d9e59b768f644cb0e0a6d4829b2214a723a6d
ashleyung <aung@sfo.yearup.org>
2023-01-17 14:21:19 -0800

    fuctioning profile

--- scripts/profile.js
+++ scripts/profile.js
@@ -1,20 +1,41 @@
 // Profile Javascript 
 
-function getUserInfo (){
-    let myHeaders = new Headers();
+// function getUserInfo (){
+//     let myHeaders = new Headers();
+//   myHeaders.append("accept", "application/json");
+//   myHeaders.append("Authorization", "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InN0cmluZyIsImlhdCI6MTY3MzU2NDU5NiwiZXhwIjoxNjczNjUwOTk2fQ.k4q_QCw48ew_zbBF6qtlNq25dhA9Oa6H1bOyNKT1LBE");
+  
+//   let requestOptions = {
+//     method: 'GET',
+//     headers: myHeaders,
+//     redirect: 'follow'
+//   };
+  
+//   fetch("https://microbloglite.herokuapp.com/api/users/string", requestOptions)
+//     .then(response => response.text())
+//     .then(result =>   document.getElementById("user-profile").innerHTML=result);
+  
+//   }
+  
+//   getUserInfo();
+
+function getUserInfo() {
+  let myHeaders = new Headers();
   myHeaders.append("accept", "application/json");
-  myHeaders.append("Authorization", "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InN0cmluZyIsImlhdCI6MTY3MzU2NDU5NiwiZXhwIjoxNjczNjUwOTk2fQ.k4q_QCw48ew_zbBF6qtlNq25dhA9Oa6H1bOyNKT1LBE");
-  
+  myHeaders.append(
+    "Authorization",
+    "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6Im1vbmtleWtpbmciLCJpYXQiOjE2NzM5OTM4ODUsImV4cCI6MTY3NDA4MDI4NX0.Pdo5f3gr7J1iZbBoz4iBXkduyOIxLKJxqB0uMvJUFhE"
+  );
+
   let requestOptions = {
-    method: 'GET',
+    method: "GET",
     headers: myHeaders,
-    redirect: 'follow'
+    redirect: "follow",
   };
-  
-  fetch("https://microbloglite.herokuapp.com/api/users/string", requestOptions)
-    .then(response => response.text())
-    .then(result =>   document.getElementById("user-profile").innerHTML=result);
-  
-  }
-  
-  getUserInfo();
+
+  fetch("https://microbloglite.herokuapp.com/api/users/monkeyking", requestOptions)
+    .then((response) => response.text())
+    .then((result) => (document.getElementById("user-profile").innerHTML = result));
+}
+
+getUserInfo();
