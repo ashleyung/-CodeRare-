57a00a6df84b7e3609a14f57cda1b69a59717e4b
ashleyung <aung@sfo.yearup.org>
2023-01-18 10:27:38 -0800

    Working on the Javascript for creating a post

--- scripts/profile.js
+++ scripts/profile.js
@@ -57,3 +57,39 @@ function getUserInfo() {
 
 getUserInfo();
 
+// Creating a Post on Profile Page 
+
+function createPost (){
+  let myHeaders = new Headers();
+myHeaders.append("accept", "application/json");
+myHeaders.append("Authorization", "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6Im1vbmtleWtpbmciLCJpYXQiOjE2NzQwNjQzMTksImV4cCI6MTY3NDE1MDcxOX0.kUa8dWhMg4884FDaJ85pQTlek7tG3sg6T56EZALRtsA");
+myHeaders.append("Content-Type", "application/json");
+
+let raw = JSON.stringify({
+  "text": "good morning to the monkey king"
+});
+
+let requestOptions = {
+  method: 'POST',
+  headers: myHeaders,
+  body: raw,
+  redirect: 'follow'
+};
+
+fetch("https://microbloglite.herokuapp.com/api/posts", requestOptions)
+  .then(response => response.text())
+  .then(result => console.log(result))
+  document.getElementById("create-post-form").innerHTML= form;
+  document.getElementById("post-title").innerHTML= title;
+  document.getElementById("post-content").innerHTML= content;
+
+}
+
+.catch((error) => {
+  console.log("Error: ", error);
+  
+ });
+}
+
+createPost();
+
