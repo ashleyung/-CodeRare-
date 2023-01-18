748f6aa0314f3bd3e37932562063cb308d568f29
ashleyung <aung@sfo.yearup.org>
2023-01-18 15:13:21 -0800

    updated profile page, sorta

--- scripts/profile.js
+++ scripts/profile.js
@@ -1,32 +1,11 @@
 // Profile Javascript 
 
-// function getUserInfo() {
-//   let myHeaders = new Headers();
-//   myHeaders.append("accept", "application/json");
-//   myHeaders.append(
-//     "Authorization",
-//     "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6Im1vbmtleWtpbmciLCJpYXQiOjE2NzM5OTM4ODUsImV4cCI6MTY3NDA4MDI4NX0.Pdo5f3gr7J1iZbBoz4iBXkduyOIxLKJxqB0uMvJUFhE"
-//   );
-
-//   let requestOptions = {
-//     method: "GET",
-//     headers: myHeaders,
-//     redirect: "follow",
-//   };
-
-//   fetch("https://microbloglite.herokuapp.com/api/users/monkeyking", requestOptions)
-//     .then((response) => response.text())
-//     .then((result) => (document.getElementById("user-profile").innerHTML = result));
-// }
-
-// getUserInfo();
-
 function getUserInfo() {
   let myHeaders = new Headers();
   myHeaders.append("accept", "application/json");
   myHeaders.append(
     "Authorization",
-    "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6Im1vbmtleWtpbmciLCJpYXQiOjE2NzM5OTM4ODUsImV4cCI6MTY3NDA4MDI4NX0.Pdo5f3gr7J1iZbBoz4iBXkduyOIxLKJxqB0uMvJUFhE"
+    "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6Im1vbmtleWtpbmciLCJpYXQiOjE2NzQwODE4NjUsImV4cCI6MTY3NDE2ODI2NX0.7-m49KRcgB1KDXf6_7HbNLOwUHvtXIvjosicgN48rz0"
   );
 
   let requestOptions = {
@@ -40,12 +19,14 @@ function getUserInfo() {
     .then((result) => {
       // Extract the relevant information from the API response
       let username = result.username;
+      let fullName = result.fullName;
       let bio = result.bio;
       let profile_picture = result.profile_picture;
       let posts = result.posts;
 
       // Insert the information into the appropriate elements in the HTML
       document.getElementById("username").innerHTML = username;
+      document.getElementById("user-fullname").innerHTML= fullName;
       document.getElementById("user-bio").innerHTML = bio;
       document.getElementById("user-pic").src = profile_picture;
       document.getElementById("user-posts").innerHTML = posts;
@@ -59,37 +40,109 @@ getUserInfo();
 
 // Creating a Post on Profile Page 
 
-function createPost (){
-  let myHeaders = new Headers();
-myHeaders.append("accept", "application/json");
-myHeaders.append("Authorization", "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6Im1vbmtleWtpbmciLCJpYXQiOjE2NzQwNjQzMTksImV4cCI6MTY3NDE1MDcxOX0.kUa8dWhMg4884FDaJ85pQTlek7tG3sg6T56EZALRtsA");
-myHeaders.append("Content-Type", "application/json");
+// function createPost (){
+//   let myHeaders = new Headers();
+// myHeaders.append("accept", "application/json");
+// myHeaders.append("Authorization", "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6Im1vbmtleWtpbmciLCJpYXQiOjE2NzQwNjQzMTksImV4cCI6MTY3NDE1MDcxOX0.kUa8dWhMg4884FDaJ85pQTlek7tG3sg6T56EZALRtsA");
+// myHeaders.append("Content-Type", "application/json");
 
-let raw = JSON.stringify({
-  "text": "good morning to the monkey king"
-});
+// let raw = JSON.stringify({
+//   "text": "good morning to the monkey king"
+// });
 
-let requestOptions = {
-  method: 'POST',
-  headers: myHeaders,
-  body: raw,
-  redirect: 'follow'
-};
+// let requestOptions = {
+//   method: 'POST',
+//   headers: myHeaders,
+//   body: raw,
+//   redirect: 'follow'
+// };
 
-fetch("https://microbloglite.herokuapp.com/api/posts", requestOptions)
-  .then(response => response.text())
-  .then(result => console.log(result))
-  document.getElementById("create-post-form").innerHTML= form;
-  document.getElementById("post-title").innerHTML= title;
-  document.getElementById("post-content").innerHTML= content;
+// fetch("https://microbloglite.herokuapp.com/api/posts", requestOptions)
+//   .then(response => response.text())
+//   .then(result => console.log(result))
+//   document.getElementById("create-post-form").innerHTML= form;
+//   document.getElementById("post-title").innerHTML= title;
+//   document.getElementById("post-content").innerHTML= content;
 
-}
+// }
 
-.catch((error) => {
-  console.log("Error: ", error);
+// .catch((error) => {
+//   console.log("Error: ", error);
   
- });
-}
+//  });
+// }
 
-createPost();
+// createPost();
 
+// function createPost(event) {
+//   console.log("SubmitEvent")
+//   event.preventDefault()
+//   let title = document.getElementById("post-title").value;
+//   let content = document.getElementById("post-content").value;
+
+//   let myHeaders = new Headers();
+//   myHeaders.append("accept", "application/json");
+//   myHeaders.append(
+//     "Authorization",
+//     "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6Im1vbmtleWtpbmciLCJpYXQiOjE2NzQwNjQzMTksImV4cCI6MTY3NDE1MDcxOX0.kUa8dWhMg4884FDaJ85pQTlek7tG3sg6T56EZALRtsA"
+//   );
+//   myHeaders.append("Content-Type", "application/json");
+
+//   let raw = JSON.stringify({
+//     title: title,
+//     content: content
+//   });
+
+//   let requestOptions = {
+//     method: "POST",
+//     headers: myHeaders,
+//     body: raw,
+//     redirect: "follow",
+//   };
+
+//   fetch("https://microbloglite.herokuapp.com/api/posts", requestOptions)
+//     .then((response) => response.json())
+//     .then((data) => {
+//       console.log(data);
+//       //Create a new div to display the post
+//       const postDiv = document.createElement("div");
+//       postDiv.classList.add("post");
+//       // Append the post title and content to the div
+//       postDiv.innerHTML = `<h2>${data.title}</h2><p>${data.content}</p>`;
+//       // Append the post div to the HTML
+//       document.getElementById("posts").appendChild(postDiv);
+
+//       const form = document.getElementById("create-post-form");
+//     form.addEventListener("submit", (event) => {
+//     event.preventDefault();
+//     createPost();
+// });
+//     })
+//     .catch((error) => {
+//       console.log("Error: ", error);
+//     });
+// }
+
+function postBtnOnClick() {
+      let inputElement = document.getElementById('post');
+      let textToPost = inputElement.value;
+      let data = { text: textToPost };
+    
+      let options = {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: {
+          "Content-Type": 'application/json',
+          "Authorization": `Bearer ${(loginData()).token}`
+        },
+      };
+    
+      fetch("https://microbloglite.herokuapp.com/api/posts", options)
+        .then(response => {
+          console.log(data)
+          if (response.ok) {
+            inputElement.value = '';
+          }
+        });
+      console.log("Post updating...")
+    }
---
+++ test.js
@@ -0,0 +1,7 @@
+let pets = [
+    { name: "Diffy", type: "Cat" },
+    { name: "Clancy", type: "Cat" },
+    { name: "Brandy", type: "Dog" },
+    { name: "Visa", type: "Dog" },
+    { name: "TK", type: "Cat" }
+];
